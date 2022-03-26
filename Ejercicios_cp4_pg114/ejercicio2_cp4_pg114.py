02e6edc27621cbad315afd53eabd408e7285ddf3
cristianascen202025 <crisfasgo20@misena.edu.co>
2022-03-26 07:45:11 -0500

    actualizacion dia 26/03/2022

--- Ejercicios_cp4_pg114/ejercicio2_cp4_pg114.py
+++ Ejercicios_cp4_pg114/ejercicio2_cp4_pg114.py
@@ -3,6 +3,5 @@ n=str(input("Introduzca una cadena de texto: "))
 for l in n:
     if  not l.isalpha():
         print(f"No es una letra ({l})")
-    
     else:
         print(f"Es una letra ({l})")
