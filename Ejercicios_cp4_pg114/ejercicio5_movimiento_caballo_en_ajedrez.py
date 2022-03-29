62b889c3f0e7da0e2615a0f44aa6a15d2565d4a7
Cristian Ascencio <101849129+cristianascenc202505@users.noreply.github.com>
2022-03-28 21:06:00 -0500

    Add files via upload

---
+++ Ejercicios_cp4_pg114/ejercicio5_movimiento_caballo_en_ajedrez.py
@@ -0,0 +1,8 @@
+x=int(input('digite el 1er punto de entrada'))
+y=int(input('digite el 2do punto de entrada'))
+
+if x>=0 and y >=0:
+    while px :
+    
+        
+    
---
+++ Ejercicios_cp4_pg114/ejercicio_profe.py
@@ -0,0 +1,11 @@
+edad= int(input('digite la edad de la persona'))
+cont=0
+h=1
+m=0
+if edad<=17:
+    print ('No sepermiten menores de edad')
+else:
+    while edad >=18:
+        nombre=input("digite el nombre completo de la persona")
+    
+    
