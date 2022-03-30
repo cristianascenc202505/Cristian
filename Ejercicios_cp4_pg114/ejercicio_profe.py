0dbf5c8aebd929f98884038e1cd48b6894393528
Cristian Ascencio <101849129+cristianascenc202505@users.noreply.github.com>
2022-03-30 15:11:42 -0500

    Add files via upload

--- Ejercicios_cp4_pg114/ejercicio_profe.py
+++ Ejercicios_cp4_pg114/ejercicio_profe.py
@@ -1,11 +1,40 @@
-edad= int(input('digite la edad de la persona'))
-cont=0
-h=1
-m=0
-if edad<=17:
-    print ('No sepermiten menores de edad')
+ingreso= str(input('Desea ingresar?: '))
+ch=0
+cm=0
+acu_h=0
+acu_m=0
+e_min= 0
+
+if ingreso=='si'or ingreso=='Si' or ingreso=='SI':
+    
+    while ingreso=='si' or ingreso=='Si' or ingreso=='SI':
+        print('\nNuevo usuario')
+        edad= int(input('Que edad tiene: '))
+        t_h_m=ch+cm
+        
+
+        if edad>= 18:
+            nombre=str(input('Nombre completo: '))
+            genero=str(input('genero: '))
+            
+            if  e_min<edad and edad !=0:
+                ed=edad            
+
+            if genero=='hombre':            
+                ch +=1
+                acu_h=acu_h+edad
+                pro_h=int(acu_h/ch)
+            elif  genero=='mujer':
+                cm +=1
+                acu_m=acu_m+edad
+                pro_m=int(acu_m/cm)
+
+        elif edad>0 and edad<18:
+            print('No puedes ingresar, no tiene la edad requirida para el ingreso')
+        else: 
+            break        
+    print(f"""\nTotal asistentes a la fiesta: {t_h_m}\nTotal asistentes hombres: {ch}.\nTotal mujeres: {cm}
+            \nPromedio edad hombre: {pro_h} \nPromedio edad mujeres: {pro_m}
+            \nEdad minima de personas que asistieron: {ed}""")
 else:
-    while edad >=18:
-        nombre=input("digite el nombre completo de la persona")
-    
-    
+    print('salir de pagina')   
