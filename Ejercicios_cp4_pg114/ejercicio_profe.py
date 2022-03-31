c26853bd9fd423265c103e84ea3dd79b4c97e87f
Cristian Ascencio <101849129+cristianascenc202505@users.noreply.github.com>
2022-03-30 20:03:23 -0500

    Add files via upload

--- Ejercicios_cp4_pg114/ejercicio_profe.py
+++ Ejercicios_cp4_pg114/ejercicio_profe.py
@@ -3,23 +3,22 @@ ch=0
 cm=0
 acu_h=0
 acu_m=0
-e_min= 0
+e_min=999
 
 if ingreso=='si'or ingreso=='Si' or ingreso=='SI':
     
     while ingreso=='si' or ingreso=='Si' or ingreso=='SI':
         print('\nNuevo usuario')
         edad= int(input('Que edad tiene: '))
-        t_h_m=ch+cm
-        
+        t_h_m=ch+cm        
 
         if edad>= 18:
             nombre=str(input('Nombre completo: '))
             genero=str(input('genero: '))
             
-            if  e_min<edad and edad !=0:
-                ed=edad            
-
+            if edad<e_min and edad !=0:
+                e_min=edad
+                
             if genero=='hombre':            
                 ch +=1
                 acu_h=acu_h+edad
@@ -35,6 +34,6 @@ if ingreso=='si'or ingreso=='Si' or ingreso=='SI':
             break        
     print(f"""\nTotal asistentes a la fiesta: {t_h_m}\nTotal asistentes hombres: {ch}.\nTotal mujeres: {cm}
             \nPromedio edad hombre: {pro_h} \nPromedio edad mujeres: {pro_m}
-            \nEdad minima de personas que asistieron: {ed}""")
+            \nEdad minima de personas que asistieron: {e_min}""")
 else:
     print('salir de pagina')   
